8bd2cb799843fcc66d1e6166c3b0335b4cfe4068
Nick <devnnick@gmail.com@>
2018-05-24 12:53:06 +0300

    modified app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { ButtonsModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { BsDropdownModule } from 'ngx-bootstrap';
-<<<<<<< HEAD
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
@@ -13,12 +12,10 @@ import { environment} from '../environments/environment';
 import {AuthService} from './services/auth.service';
 import {LocationStrategy} from '@angular/common';
 import {HashLocationStrategy} from '@angular/common';
-=======
 import { FrontPageComponent } from './front-page/front-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserPageComponent } from './user-page/user-page.component';
 
->>>>>>> a8f51eb2f4f072beaec87e21f651426c295ad6c1
 @NgModule({
   declarations: [
     AppComponent,
