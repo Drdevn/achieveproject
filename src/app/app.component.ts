cd46d3eda787c88cdfd6f1bdc92ce7949762bc20
nazarmentukh <decster1488@gmail.com>
2018-05-25 15:45:04 +0300

    sung up upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { UserService} from './services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,49 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 export class AppComponent {
   title = 'app';
 
+
+  public users;
+  public username;
+  public email;
+  public password;
+  public validpassword;
+  postusers = [];
   modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  
+  constructor(private modalService: BsModalService, 
+    protected userserv: UserService, private router: Router) {}
 
   openModal(template) {
     this.modalRef = this.modalService.show(template);
   }
+
+  ngOnInit() {
+
+    this.userserv.getUsers().subscribe(data=>{
+      this.users = data;
+    })
+  }
+  postreqverif(){
+   let userdata;
+   if(this.password == this.validpassword){
+    userdata = { username: this.username, email: this.email, password: this.password, counter: "0" }
+    this.userserv.addUser(userdata, '/add').subscribe(res =>{});
+    this.cleaner();
+    console.log(userdata);
+    }
+    else{
+      alert("Your password don`t match");
+      this.cleaner();
+    }
+
+  }
+
+  cleaner(){
+    this.username = "";
+    this.password = "";
+    this.email = "";
+    this.validpassword = "";
+  }
+
  
 }
