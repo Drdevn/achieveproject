fd452f41e4f44b9d0ce9f2f5ddc3ad734fefdbc0
Nick <devnnick@gmail.com@>
2018-05-29 17:51:00 +0300

    added auth guard v 0.1

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { UserService} from './services/user.service';
+import { User } from 'firebase';
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private userservice: UserService,
+              private router: Router){}
+
+  canActivate(): boolean {
+  if(this.userservice.loggedIn()) {
+    return true
+  } else {
+    this.router.navigate(['/login'])
+    return false
+   }
+  }
+}
