0a2460ada09f9484c7e4279f40da8db2c3c81e2c
decster1488 <decster1488@gmail.com>
2018-06-05 16:05:45 +0300

    test 2.0

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { UserService } from '../services/user.service';
-import * as decode from 'jwt-decode'
-import { Subject } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {BsModalService} from 'ngx-bootstrap/modal';
+import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import {UserService} from '../services/user.service';
+import * as decode from 'jwt-decode';
+import {Subject} from 'rxjs';
 
 
 @Component({
@@ -17,51 +17,53 @@ export class UserPageComponent implements OnInit {
   public iconeList;
   public changeIcon = this.userIcon;
   public updcounter;
-  public updurl;
+  public updurl;gi
   public userId;
   modalRef: BsModalRef;
-  
+
   public token = localStorage.getItem('token');
   public tokenPayload = <any>{};
-  
-  constructor( private modalService: BsModalService, private userService: UserService ) { }
-  
-  
-  
+
+  constructor(private modalService: BsModalService, private userService: UserService) {
+  }
+
+
   ngOnInit() {
-    // Дістаємо базу аватарів СТАРТ 
-    this.userService.getIcons().subscribe(data =>{
+    // Дістаємо базу аватарів СТАРТ
+    this.userService.getIcons().subscribe(data => {
       this.iconeList = data;
       console.log(this.iconeList[0].name);
     });
     // Дістаємо базу аватарів КІНЕЦЬ
     this.tokenPayload = decode(this.token);
-    let userid = {id:this.tokenPayload.subject}
-    this.userService.getUser(userid).subscribe(data => { 
+    const userid = {id: this.tokenPayload.subject};
+    this.userService.getUser(userid).subscribe(data => {
       this.userData = data;
       this.userIcon = data.icon;
       this.changeIcon = this.userIcon;
-    })
-      
+    });
+
     console.log(this.userIcon);
-    }
-    
-    openModal(template) {
-      this.modalRef = this.modalService.show(template);
   }
-  
-  setNewIcon(){
+
+  openModal(template) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  setNewIcon() {
     this.userIcon = this.changeIcon;
-    let userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.userIcon}
-    this.userService.updateUser(userupd).subscribe(res =>{})
+    const userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.userIcon};
+    this.userService.updateUser(userupd).subscribe(res => {
+    });
     this.modalRef.hide();
 
   }
-  updateUser(){
-    let userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.updurl}
-    this.userService.updateUser(userupd).subscribe(res =>{})
+
+  updateUser() {
+    const userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.updurl};
+    this.userService.updateUser(userupd).subscribe(res => {
+    });
   }
-  
-  
- 
+
+
 }
