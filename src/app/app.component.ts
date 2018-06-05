ac4fdf583221a96c66c770bde7add317edbcf7a9
decster1488 <decster1488@gmail.com>
2018-06-05 17:11:17 +0300

    home button update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, TemplateRef } from '@angular/core';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { UserService} from './services/user.service';
-import { Router } from '@angular/router';
+import {Component, TemplateRef} from '@angular/core';
+import {BsModalService} from 'ngx-bootstrap/modal';
+import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import {UserService} from './services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -21,8 +21,9 @@ export class AppComponent {
 
   modalRef: BsModalRef;
 
-  constructor(private modalService: BsModalService, 
-    protected userserv: UserService, private router: Router) {}
+  constructor(private modalService: BsModalService,
+              protected userserv: UserService, private router: Router) {
+  }
 
   openModal(template) {
     this.modalRef = this.modalService.show(template);
@@ -30,48 +31,50 @@ export class AppComponent {
 
   ngOnInit() {
 
-    
-  }
-  
 
-  cleaner(){
-    this.username = "";
-    this.password = "";
-    this.email = "";
-    this.validpassword = "";
   }
 
-  registerUser(){
+
+  cleaner() {
+    this.username = '';
+    this.password = '';
+    this.email = '';
+    this.validpassword = '';
+  }
+
+  registerUser() {
     let userdata;
-    userdata = { username: this.username,
-       email: this.email,
-       password: this.password, counter: "0", icon:"avatar.svg" }
+    userdata = {
+      username: this.username,
+      email: this.email,
+      password: this.password, counter: '0', icon: 'avatar.svg'
+    };
     this.userserv.registerUser(userdata)
-    .subscribe(
-      res => {
-        console.log(res)
-        localStorage.setItem('token', res.token)
-        this.router.navigate(['/userpage'])      
-        
-      },  
-      err => console.log(err)
-    )
+      .subscribe(
+        res => {
+          console.log(res);
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/userpage']);
+
+        },
+        err => console.log(err)
+      );
     this.modalRef.hide();
     this.cleaner();
   }
- 
-  loginUser(){
+
+  loginUser() {
     let loginData;
-    loginData = { email: this.email, password: this.password}
+    loginData = {email: this.email, password: this.password};
     this.userserv.loginUser(loginData)
-    .subscribe(
-      res =>{ 
-      console.log(res)
-      localStorage.setItem('token', res.token)
-      this.router.navigate(['/userpage'])      
-    },
-      err => console.log(err)
-    )
+      .subscribe(
+        res => {
+          console.log(res);
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/userpage']);
+        },
+        err => console.log(err)
+      );
     this.modalRef.hide();
     this.cleaner();
   }
--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -3,7 +3,7 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { UserService } from '../services/user.service';
 import * as decode from 'jwt-decode';
-import { Subject } from 'rxjs';
+
 
 
 @Component({
@@ -36,7 +36,7 @@ export class UserPageComponent implements OnInit {
     });
     // Дістаємо базу аватарів КІНЕЦЬ
     this.tokenPayload = decode(this.token);
-    let userid = {id:this.tokenPayload.subject}
+    const userid = {id:this.tokenPayload.subject};
     this.userService.getUser(userid).subscribe(data => {
       this.userData = data;
       this.userIcon = data.icon;
@@ -52,13 +52,13 @@ export class UserPageComponent implements OnInit {
 
   setNewIcon(){
     this.userIcon = this.changeIcon;
-    let userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.userIcon}
+    const userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.userIcon}
     this.userService.updateUser(userupd).subscribe(res =>{})
     this.modalRef.hide();
 
   }
   updateUser(){
-    let userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.updurl}
+    const userupd = {id: this.tokenPayload.subject, counter: this.updcounter, icon: this.updurl}
     this.userService.updateUser(userupd).subscribe(res =>{})
   }
 
